931159edeb5a5d369eb3bdcdd329d65148fc176a
AlonaAkimova <aliona.akimova@gmail.com>
2023-06-13 15:09:57 +0700

    updates

--- src/js/model.js
+++ src/js/model.js
@@ -1,15 +1,16 @@
 import { async } from 'regenerator-runtime';
-
+import { API_URL } from './config.js';
+import { getJSON } from './helpers.js';
 export const state = {
   recipe: {},
+  search: {
+    query: '',
+    results: [],
+  },
 };
 export const loadRecipe = async function (id) {
   try {
-    const res = await fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-    );
-    const data = await res.json();
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    const data = await getJSON(`${API_URL}${id}`);
     const { recipe } = data.data;
     state.recipe = {
       id: recipe.id,
@@ -23,6 +24,24 @@ export const loadRecipe = async function (id) {
     };
     console.log(recipe);
   } catch (err) {
-    alert(err);
+    throw err;
+  }
+};
+
+export const loadSearchResults = async function (query) {
+  try {
+    state.search.query = query;
+    const data = await getJSON(`${API_URL}?search=${query}`);
+    console.log(data);
+    state.search.results = data.data.recipes.map(rec => {
+      return {
+        id: rec.id,
+        title: rec.title,
+        publisher: rec.publisher,
+        image: rec.image_url,
+      };
+    });
+  } catch {
+    throw err;
   }
 };
--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,38 +1,24 @@
-import icons from 'url:../../img/icons.svg';
-// import icons from '../img/icons.svg';
-class RecipeView {
-  #parentElement = document.querySelector('.recipe');
-  #data;
-  render(data) {
-    this.#data = data;
-    const markup = this.#generateMarkup;
-    this.#clear;
-    this.#parentElement.insertAdjacentHTML('afterbegin', markup);
+// import icons from 'url:../../img/icons.svg';
+import View from './View.js';
+import icons from '../../img/icons.svg';
+import { Fraction } from 'fractional';
+
+class RecipeView extends View {
+  _parentElement = document.querySelector('.recipe');
+  _errorMessage = 'We could not find that recipe. Please try another one! 😖';
+  _message = '';
+
+  addHandlerRender(handler) {
+    ['hashchange', 'load'].forEach(ev => window.addEventListener(ev, handler));
   }
 
-  #clear() {
-    this.#parentElement.innerHTML = '';
-  }
-
-  renderSpinner = function () {
-    const markup = `
-  <div class="spinner">
-            <svg>
-              <use href="${icons}#icon-loader"></use>
-            </svg>
-          </div>
-  `;
-    this.#parentElement.innerHTML = '';
-    this.#parentElement.insertAdjacentHTML('afterbegin', markup);
-  };
-
-  #generateMarkup() {
+  _generateMarkup() {
     return `
     <figure class="recipe__fig">
-          <img src="${this.#data.image}" alt=${
-      this.#data.title
+          <img src="${this._data.image}" alt=${
+      this._data.title
     }class="recipe__img" />
-          <h1 class="${this.#data.title}">
+          <h1 class="${this._data.title}">
             <span>Pasta with tomato cream sauce</span>
           </h1>
         </figure>
@@ -43,7 +29,7 @@ class RecipeView {
               <use href="${icons}#icon-clock"></use>
             </svg>
             <span class="recipe__info-data recipe__info-data--minutes">${
-              this.#data.cookingTime
+              this._data.cookingTime
             }</span>
             <span class="recipe__info-text">minutes</span>
           </div>
@@ -52,7 +38,7 @@ class RecipeView {
               <use href="${icons}#icon-users"></use>
             </svg>
             <span class="recipe__info-data recipe__info-data--people">4</span>
-            <span class="recipe__info-text">${this.#data.servings}</span>
+            <span class="recipe__info-text">${this._data.servings}</span>
 
             <div class="recipe__info-buttons">
               <button class="btn--tiny btn--increase-servings">
@@ -69,9 +55,7 @@ class RecipeView {
           </div>
 
           <div class="recipe__user-generated">
-            <svg>
-              <use href="${icons}#icon-user"></use>
-            </svg>
+          
           </div>
           <button class="btn--round">
             <svg class="">
@@ -83,22 +67,7 @@ class RecipeView {
         <div class="recipe__ingredients">
           <h2 class="heading--2">Recipe ingredients</h2>
           <ul class="recipe__ingredient-list">
-          ${this.#data.ingredients
-            .map(ing => {
-              return `
-            <li class="recipe__ingredient">
-              <svg class="recipe__icon">
-                <use href="${icons}#icon-check"></use>
-              </svg>
-              <div class="recipe__quantity">${ing.quantity}</div>
-              <div class="recipe__description">
-                <span class="recipe__unit">${ing.unit}</span>
-                ${ing.description}
-              </div>
-            </li>
-            `;
-            })
-            .join('')}
+          ${this._data.ingredients.map(this._generateMarkupIngridient).join('')}
         </div>
 
         <div class="recipe__directions">
@@ -106,13 +75,13 @@ class RecipeView {
           <p class="recipe__directions-text">
             This recipe was carefully designed and tested by
             <span class="recipe__publisher">${
-              this.#data.publisher
+              this._data.publisher
             }</span>. Please check out
             directions at their website.
           </p>
           <a
             class="btn--small recipe__btn"
-            href="${this.#data.sourceUrl}"
+            href="${this._data.sourceUrl}"
             target="_blank"
           >
             <span>Directions</span>
@@ -121,6 +90,22 @@ class RecipeView {
             </svg>
           </a>
         </div>
+    `;
+  }
+  _generateMarkupIngridient(ing) {
+    return `
+    <li class="recipe__ingredient">
+      <svg class="recipe__icon">
+        <use href="${icons}#icon-check"></use>
+      </svg>
+      <div class="recipe__quantity">${
+        ing.quantity ? new Fraction(ing.quantity).toString() : ''
+      }</div>
+      <div class="recipe__description">
+        <span class="recipe__unit">${ing.unit}</span>
+        ${ing.description}
+      </div>
+    </li>
     `;
   }
 }
