0dfb2c1288cf36b4bf5d6fd2a372c7ed4daff5f5
AlonaAkimova <aliona.akimova@gmail.com>
2023-06-15 13:24:52 +0700

    changes

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = '9cd32f33-6fb2-405e-b8fc-962ad948a679';
+export const MODAL_CLOSE_SEC = 2.5;
